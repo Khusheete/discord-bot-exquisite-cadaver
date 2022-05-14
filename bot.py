e81dbb25a8933ae24269f39dbb644c9ca4bfa33f
Siandfrance <Siandfrance@gmail.com>
2022-05-14 20:08:52 +0200

    Initial Commit

---
+++ bot.py
@@ -0,0 +1,222 @@
+import discord
+import game
+
+
+ENV: dict
+HELP: str
+
+with open("env.json", "r") as f:
+    import json
+    ENV = json.load(f)
+
+with open("help.txt") as f:
+    HELP = f.read()
+
+
+client = discord.Client()
+current_games: list[game.ExquisiteCadaver] = []
+
+
+@client.event
+async def on_ready():
+    print(f'{client.user} connected to Discord!')
+
+
+@client.event
+async def on_message(message):
+    #ignore messages from self
+    if message.author == client.user:
+        return
+
+    #it is a game command
+    
+    if isinstance(message.channel, discord.channel.DMChannel) or message.content.startswith("ExquisiteCadaver") or message.content.startswith("!ec"):
+        if message.content.startswith("ExquisiteCadaver") or message.content.startswith("!ec"):
+            command: list[str] = message.content.split(" ")[1::]
+        else:
+            command: list[str] = message.content.split(" ")
+
+        if len(command) == 0 or command[0] == "help":
+            await message.channel.send(HELP)
+        elif command[0] == "create":
+            if len(command) < 2:
+                await message.channel.send("Specify at least the mode of the game (modes are \"whole_sentence\", \"last_words\" and \"first_words\").")
+                await message.channel.send(HELP)
+                return
+            #check if the author already has a game at his name
+            for g in current_games:
+                if g.game_admin == message.author:
+                    await message.channel.send("You already have created an Exquisite Cadaver (id: {}]).".format(g.game_id))
+                    return
+            
+            mode: game.GameMode
+            character_limit: int = 120
+            word_count: int = 3
+
+            if command[1] == "whole_sentence":
+                mode = game.GameMode.WHOLE_SENTENCE
+            elif command[1] == "last_words":
+                mode = game.GameMode.LAST_WORDS
+            elif command[1] == "first_words":
+                mode = game.GameMode.FIRST_WORDS
+            elif command[1] == "random_words":
+                mode = game.GameMode.RANDOM_WORDS
+            
+            if len(command) >= 3:
+                try:
+                    character_limit = int(command[2])
+                except Exception:
+                    pass
+            if len(command) >= 4:
+                try:
+                    word_count = int(command[3])
+                except Exception:
+                    pass
+            
+            g = game.ExquisiteCadaver(mode, message.author, word_count, character_limit)
+            current_games.append(g)
+
+            await message.channel.send(f"Created {message.author}'s game with id {g.game_id}.\nIf you want to join this game enter the command \"join {g.game_id}\"")
+        elif command[0] == "end" or command[0] == "stop":
+            if len(command) < 2:
+                await message.channel.send("Specify game id for the game you want to terminate.")
+                await message.channel.send(HELP)
+                return
+            
+            game_id: int
+
+            try:
+                game_id = int(command[1])
+            except Exception:
+                await message.channel.send(f"Could not read id \"{command[1]}\"")
+                return
+
+            for g in current_games:
+                #check for the author
+                if g.game_id == game_id:
+                    if g.game_admin == message.author:
+                        if g.started():
+                            #print the result of the game
+                            mentions: str = ""
+                            for i in range(len(g.participents)):
+                                mentions += f"<@{g.participents[i].id}>"
+                                if i + 1 < len(g.participents):
+                                    mentions += ", "
+                            await message.channel.send(f"{mentions} The game {g.game_id} from {g.game_admin} was {g.current_turn} turn long and the result is:")
+                            print("\r\nGame {g.game_id}:\r\n")
+                            for tale in g.get_tale():
+                                print(f"\x1b[1m{tale}\x1b[m\r\n")
+                                await message.channel.send(f"\r\n{tale}")
+                        else:
+                            await message.channel.send(f"Deleted game {g.game_id}.")
+                        current_games.remove(g)
+                        return
+                    else:
+                        await message.channel.send(f"You do not own the game with id {game_id}.")
+                        return
+
+            await message.channel.send(f"There is no game with id {game_id}.")
+        elif command[0] == "join":
+            if len(command) < 2:
+                await message.channel.send("Specify game id for the game you want to join.")
+                await message.channel.send(HELP)
+                return
+            
+            game_id: int
+
+            try:
+                game_id = int(command[1])
+            except Exception:
+                await message.channel.send(f"Could not read id \"{command[1]}\"")
+                return
+
+            for g in current_games:
+                if g.game_id == game_id:
+                    error = g.register_participent(message.author)
+                    if error == game.GameError.ALREADY_JOINED:
+                        await message.channel.send(f"You already joined the game {g.game_id}.")
+                    elif error == game.GameError.ALREADY_STARTED:
+                        await message.channel.send(f"The game {g.game_id} was already started.")
+                    elif error == game.GameError.OK:
+                        await message.add_reaction("👍")
+                    return
+            
+            await message.channel.send(f"Could not find the game {game_id}.")
+        elif command[0] == "start":
+            if len(command) < 2:
+                await message.channel.send("Specify game id for the game you want to start.")
+                await message.channel.send(HELP)
+                return
+
+            game_id: int
+
+            try:
+                game_id = int(command[1])
+            except Exception:
+                await message.channel.send(f"Could not read id \"{command[1]}\"")
+                return
+
+            for g in current_games:
+                if g.game_id == game_id:
+                    if g.game_admin == message.author:
+                        error = g.start_game()
+                        if error == game.GameError.NOT_ENOUGH_PLAYERS:
+                            await message.channel.send(f"There are not enough players to start game {g.game_id}.")
+                        elif error == game.GameError.ALREADY_STARTED:
+                            await message.channel.send(f"The game {g.game_id} was already started.")
+                        elif error == game.GameError.OK:
+                            mentions: str = ""
+                            for i in range(len(g.participents)):
+                                mentions += f"<@{g.participents[i].id}>"
+                                if i + 1 < len(g.participents):
+                                    mentions += ", "
+                            await message.channel.send(f"{mentions} Started game {g.game_id}.")
+                            print(f"[{g.get_next_player().name}] Now writing")
+                            await g.get_next_player().send(f"It is your turn to add a sentence to the game {g.game_id} by {g.game_admin} using the command \"post {g.game_id} <your sentence>\".")
+                    else:
+                        await message.channel.send(f"You do not own the game with id {game_id}.")
+                    return
+            
+            await message.channel.send(f"Could not find the game {game_id}.")
+        elif command[0] == "post":
+            if len(command) < 3:
+                await message.channel.send("Specify game id for the game you want to post to and the sentence you want to write.")
+                await message.channel.send(HELP)
+                return
+            
+            game_id: int
+
+            try:
+                game_id = int(command[1])
+            except Exception:
+                await message.channel.send(f"Could not read id \"{command[1]}\"")
+                return
+
+            if message.content.startswith("ExquisiteCadaver") or message.content.startswith("!ec"):
+                sentence: str = message.content.split(" ", 3)[3]
+            else:
+                sentence: str = message.content.split(" ", 2)[2]
+
+            for g in current_games:
+                if g.game_id == game_id:
+                    error = g.push_sentence(message.author, sentence + " ")
+                    if error == game.GameError.WRONG_PARTICIPENT:
+                        print(f"[{message.author.name}] Wrong Participent")
+                        await message.author.send("It is not your turn to post a sentence.")
+                    elif error == game.GameError.TOO_MUCH_CHARS:
+                        print(f"[{message.author.name}] Too much chars")
+                        await message.author.send(f"The maximum number of characters you can post in this game is {g.character_limit}.")
+                    elif error == game.GameError.NOT_STARTED:
+                        print(f"[{message.author.name}] Not Started")
+                        await message.author.send("This game was not started yet.")
+                    elif error == game.GameError.OK:
+                        print(f"[{message.author.name}] Posted sucessfully")
+                        await message.add_reaction("👍")
+                        print(f"\n[{g.get_next_player().name}] Now writing")
+                        await g.get_next_player().send(f"It is your turn to add a sentence to the game {g.game_id} by {g.game_admin} using the command \"post {g.game_id} <your sentence>\".\nThis is your indication to write: \n\"{g.get_given_info()}\"")
+                    return
+            
+            await message.channel.send(f"Could not find the game {game_id}.")
+
+
+client.run(ENV["discord_token"])
---
+++ game.py
@@ -0,0 +1,179 @@
+from random import randint, choice
+from enum import Enum
+from string import punctuation, whitespace
+import re
+
+WORDS = re.compile(f"[-{punctuation.replace('-', '')}{whitespace}]*[-'\\w]+(?:\\b|$)")
+LAST_PONCTUATION = re.compile(f"\\b[-{punctuation.replace('-', '')}{whitespace}]*$(?:[\r\n]|\r\n)?")
+
+class GameMode(Enum):
+    WHOLE_SENTENCE: int = 0
+    FIRST_WORDS: int = 1
+    LAST_WORDS: int = 2
+    RANDOM_WORDS: int = 3
+
+
+class GameError(Enum):
+    OK: int = 0
+
+    ALREADY_STARTED: int = 1
+    NOT_ENOUGH_PLAYERS: int = 2
+
+    TOO_MUCH_CHARS: int = 3
+    WRONG_PARTICIPENT: int = 4
+
+    ALREADY_JOINED: int = 5
+    NOT_STARTED: int = 6
+
+
+def get_cut_sentence(sentence: str, pos: int = 0, count: int = 1) -> str:
+    result: str = ""
+    words: list[str] = decompose_words(sentence)
+    
+    if count >= 0:
+        for i in range(pos, pos + count):
+            if i >= len(words):
+                break
+            result += words[i]
+    
+        if pos + count - 1 >= len(words):
+            for end in LAST_PONCTUATION.findall(sentence):
+                result += end
+    if count <= 0:
+        for i in range(pos + count + 1, pos + 1):
+            if i < 0:
+                continue
+            if i >= len(words):
+                break
+            result += words[i]
+
+        if pos + 1 >= len(words):
+            for end in LAST_PONCTUATION.findall(sentence):
+                result += end
+    
+    return result
+
+
+def decompose_words(sentence: str) -> list[str]:
+    return WORDS.findall(sentence)
+
+
+class ExquisiteCadaver:
+
+    game_id: int
+    participents: list
+
+    mode: GameMode
+    word_count: int
+    character_limit: int
+
+    current_turn: int = -1
+    order_of_participents: list[int] = []
+
+    tale: list = []
+    given_info: str = ""
+
+
+    def __init__(self, game_mode: GameMode, game_admin, word_count: int = 3, character_limit: int = 120):
+        #get 64 bit game id
+        self.game_id = randint(0, 0xffffffffffffffff)
+        self.participents = []
+        self.game_admin = game_admin
+
+        self.mode = game_mode
+        self.word_count = word_count
+        self.character_limit = character_limit
+
+        self.current_turn = -1
+        self.order_of_participents = []
+        self.tale = []
+        self.given_info = ""
+
+
+    def register_participent(self, participent) -> GameError:
+        if self.has_joined(participent):
+            return GameError.ALREADY_JOINED
+        if self.started():
+            return GameError.ALREADY_STARTED
+        self.participents.append(participent)
+        return GameError.OK
+
+
+    def is_game(self, game_id: int) -> bool:
+        return self.game_id == game_id
+
+
+    def get_game_id(self) -> int:
+        return self.game_id
+
+
+    def started(self):
+        return self.current_turn != -1
+
+
+    def has_joined(self, player) -> bool:
+        return player in self.participents
+
+
+    def start_game(self) -> GameError:
+        #ignore command if the game has been initiamized
+        if self.started():
+            return GameError.ALREADY_STARTED
+        if len(self.participents) <= 1:
+            return GameError.NOT_ENOUGH_PLAYERS
+        indecies: list[int] = [i for i in range(len(self.participents))]
+        
+
+        while len(indecies) > 0:
+            next_p: int = choice(indecies)
+            self.order_of_participents.append(next_p)
+            indecies.remove(next_p)
+
+        self.current_turn = 0
+        return GameError.OK
+
+
+    def get_next_player(self):
+        return self.participents[self.order_of_participents[self.current_turn % len(self.order_of_participents)]]
+    
+
+    def push_sentence(self, participent, sentence: str) -> GameError:
+        if not self.started():
+            return GameError.NOT_STARTED
+        if participent != self.get_next_player():
+            return GameError.WRONG_PARTICIPENT
+        if len(sentence) > self.character_limit:
+            return GameError.TOO_MUCH_CHARS
+        
+        self.tale += [sentence]
+        self.current_turn += 1
+
+        words: list[str] = decompose_words(sentence)
+
+        if self.mode == GameMode.WHOLE_SENTENCE:
+            self.given_info = sentence
+        elif self.mode == GameMode.FIRST_WORDS:
+            self.given_info = get_cut_sentence(sentence, count = self.word_count)
+        elif self.mode == GameMode.LAST_WORDS:
+            self.given_info = get_cut_sentence(sentence, pos = len(words)-1, count = -self.word_count)
+        elif self.mode == GameMode.RANDOM_WORDS:
+            self.given_info = get_cut_sentence(sentence, pos = randint(0, len(words) - self.word_count), count = self.word_count)
+
+        return GameError.OK
+    
+
+    def get_tale(self, char_limit: int = 1500):
+        res: list = []
+        curr: str = ""
+        for t in self.tale:
+            if len(curr) + len(t) > char_limit:
+                res += [curr]
+                curr = t
+            else:
+                curr += t
+        res += [curr]
+        return res
+
+
+    def get_given_info(self) -> str:
+        return self.given_info
